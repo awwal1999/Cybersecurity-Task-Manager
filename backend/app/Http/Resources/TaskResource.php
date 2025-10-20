35eb87c4ce1cbac8c8cd7a5a84a9d3e2bcad54f5
Lawal Akanbi <awwal1999@gmail.com>
2025-10-20 21:48:00 +0200

    add task resource

---
+++ backend/app/Http/Resources/TaskResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'status' => $this->status->value,
+            'due_date' => $this->due_date?->format('Y-m-d'),
+            'completed_at' => $this->completed_at?->format('Y-m-d H:i:s'),
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
+            'activities_count' => $this->whenLoaded('activities', function () {
+                return $this->activities->count();
+            }),
+            'is_completed' => $this->isCompleted(),
+            'is_overdue' => $this->isOverdue(),
+        ];
+    }
+}
